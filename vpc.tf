0890cfa0cc1cade203f35482a09e32faee0fe4d5
ravikaran90 <ravikaran90@yahoo.com>
2022-03-20 23:59:37 -0400

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,29 @@
-//VPC Creation
+//VPC and Subnet Creation
+module "vpc" {
+	source = "terraform-google-modules/network/google"
+	version = "~>4.0"
+	
+	project_id = var.project_id
+	network_name = "sonarqube-network-2"
+	
+	subnets = [
+	{
+		subnet_name = "sonarqube-subnet"
+		subnet_ip = "10.77.0.0/16"
+		subnet_region = "us-central1"
+	},
+	]
+	secondary_ranges = {
+		sonarqube-subnet
+			{
+				range_name = var.pods-subnet
+				ip_cidr_range = "10.78.0.0/16"
+			},
+			{
+				range_name = var.services-subnet
+				ip_cidr_range = "10.79.0.0/16"
+			}
+		]
+	}
+}
+
